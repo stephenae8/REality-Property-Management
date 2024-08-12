969e9c8e66e389429fdefde73649ee119d07c542
Stephen Aekins <stephenlaekins@gmail.com>
2024-08-12 16:19:11 -0400

    api and data change

---
+++ vue/src/components/test-register-page/AlertMessage.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="alert" role="alert">
+      {{ message }}
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      message: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .alert {
+    background-color: #f8d7da;
+    color: #721c24;
+    padding: 0.75rem;
+    margin-bottom: 1rem;
+    border: 1px solid #f5c6cb;
+    border-radius: 4px;
+  }
+  </style>
